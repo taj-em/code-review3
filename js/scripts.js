3b61f36e5e1c725c99c950574b86bcad9a3d9eee
Your Name <tajempey@gmail.com>
2023-10-27 10:53:42 -0600

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,21 @@ function processNum(num) {
   const targetArray = [0, 1, 2, 3];
   const responseArray = [0, "hello one", "hello two", "hello three"]
   const numArray = num.toString().split("");
+  let finalArray = [];
   console.log("Num Array : " + numArray);
+  //logs every number from 0 to the input
   for (let index = 0; index <= num; index += 1) {
-      console.log(index);
-      if (targetArray.includes(index)) {
-        console.log(responseArray[index]);
+    let indexArray = index.toString().split("");
+    console.log("Index Array: " + indexArray)
+    //
+    for (let digit = 0; digit < indexArray.length; digit +=1) {
+      if(targetArray.includes(indexArray[digit])) {
+        finalArray.push(responseArray[indexArray[digit]])
+        console.log("Final Array: " + finalArray)
       }
     };
 }
-
+}
 
 function display(event) {
   event.preventDefault();
