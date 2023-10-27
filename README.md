ad0bbe4522785e712f18ee09b645de84e4fb2ba4
Your Name <tajempey@gmail.com>
2023-10-27 11:51:47 -0600

    Log 'hello two' instead of 'hello one' when the input is 12

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Expected Result: [0, 1, 2, 3, 4, 5]
 Test: "Log a unique string when 1, 2, or 3 get logged"
 Code: processNum(5);
 Expected Result: [0, "hello one", "hello two", "hello three", 4, 5]
+
+Test: "Log "hello two" instead of "hello one" when the input is 12"
+Code: processNum(12);
+Expected Result: [... "hello one"(11), "hellow two"(12)];
--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,39 @@
 function processNum(num) {
-  const targetArray = [0, 1, 2, 3];
+  const targetArray = ["0", "1", "2", "3"];
   const responseArray = [0, "hello one", "hello two", "hello three"]
-  const numArray = num.toString().split("");
   let finalArray = [];
-  console.log("Num Array : " + numArray);
   //logs every number from 0 to the input
   for (let index = 0; index <= num; index += 1) {
     let indexArray = index.toString().split("");
-    console.log("Index Array: " + indexArray)
-    //
-    for (let digit = 0; digit < indexArray.length; digit +=1) {
-      if(targetArray.includes(indexArray[digit])) {
-        finalArray.push(responseArray[indexArray[digit]])
-        console.log("Final Array: " + finalArray)
-      }
-    };
+    if (indexArray[0] == 3) {
+      finalArray.push(responseArray[3]);
+    } else if (indexArray[1] == 3) {
+      finalArray.push(responseArray[3]);
+    } else if (indexArray[0] == 2) {
+      finalArray.push(responseArray[2]);
+    } else if (indexArray[1] == 2) {
+      finalArray.push(responseArray[2]);
+    } else if (indexArray[0] == 1) {
+      finalArray.push(responseArray[1]);
+    } else if (indexArray[1] == 1) {
+      finalArray.push(responseArray[1]);
+    } else {
+      finalArray.push(index);
+    }
+  }
+  console.log(finalArray)
 }
-}
-
-function display(event) {
-  event.preventDefault();
-  const number = document.getElementById("num-input").value;
-  const output = processNum(number);
-  document.getElementById("display").innerText = output;
-}
+  function display(event) {
+    event.preventDefault();
+    const number = document.getElementById("num-input").value;
+    const output = processNum(number);
+    document.getElementById("display").innerText = output;
+  }
 
 
-window.addEventListener("load", function() {
-this.document.querySelector("form").addEventListener("submit", display)
-});
+  window.addEventListener("load", function () {
+    this.document.querySelector("form").addEventListener("submit", display)
+  });
 
 
 
@@ -37,5 +42,4 @@ this.document.querySelector("form").addEventListener("submit", display)
 //   console.log(index);
 //   if (targetArray.includes(index)) {
 //     console.log("hello");
-//   }
-// };
+
